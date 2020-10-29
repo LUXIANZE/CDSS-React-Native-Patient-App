1beab94174b704ec2beefd8242c8b44793588f89
LUXIANZE <luxianze98@gmail.com>
2020-10-29 12:34:43 +0800

    Adjusted Project Structure - Moved App.js into a centralised src file that will contain all RN components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app, Ayyo!</Text>
       <StatusBar style="auto" />
     </View>
   );
